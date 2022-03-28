5d2e227cebcac4fd944cf7b5c002faf376f61328
JulieMeij <54932056+JulieMeij@users.noreply.github.com>
2022-03-28 14:32:07 +0200

    werkt niet

--- app/controllers/usercontroller.php
+++ app/controllers/usercontroller.php
@@ -1,43 +1,43 @@
 <?php
-// namespace Controllers;
-// use Exception;
-// use Services\UserService;
-// use Firebase\JWT\JWT;
-// use Firebase\JWT\Key;
+namespace Controllers;
+use Exception;
+use Services\UserService;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
-// class UserController extends Controller
-// {
-//     private $service;
-//     // initialize services
-//     function __construct()
-//     {
-//         $this->service = new UserService();
-//     }
-//     public function login() {
-//         try {
-//             $postedUser = $this->createObjectFromPostedJson("Models\\User");
-//             $user = $this->service->checkUsernamePassword($postedUser->username, $postedUser->password);
-//             if(!$user) {
-//                 $this->respondWithError(401, "Invalid credentials");
-//                 return false;
-//             }
-//             $key = "thismustbesecret";
-//             $payload = array(
-//                 "iss" => "http://localhost",
-//                 "aud" => "http://localhost",
-//                 "iat" => time(),
-//                 "nbf" => time(),
-//                 "exp" => time() + 600,
-//                 "data" => array(
-//                     "id" => $user->id,
-//                     "username" => $user->username
-//                 ));
-//             $jwt = JWT::encode($payload, $key, 'HS256');
+class UserController extends Controller
+{
+    private $service;
+    // initialize services
+    function __construct()
+    {
+        $this->service = new UserService();
+    }
+    public function login() {
+        try {
+            $postedUser = $this->createObjectFromPostedJson("Models\\User");
+            $user = $this->service->checkUsernamePassword($postedUser->username, $postedUser->password);
+            if(!$user) {
+                $this->respondWithError(401, "Invalid credentials");
+                return false;
+            }
+            $key = "thismustbesecret";
+            $payload = array(
+                "iss" => "http://localhost",
+                "aud" => "http://localhost",
+                "iat" => time(),
+                "nbf" => time(),
+                "exp" => time() + 600,
+                "data" => array(
+                    "id" => $user->id,
+                    "username" => $user->username
+                ));
+            $jwt = JWT::encode($payload, $key, 'HS256');
 
-//             $this->respond(["token" => $jwt]);
+            $this->respond(["token" => $jwt]);
 
-//         } catch (Exception $e) {
-//             $this->respondWithError(500, $e->getMessage());
-//         }
-//     }
-// }
+        } catch (Exception $e) {
+            $this->respondWithError(500, $e->getMessage());
+        }
+    }
+}
